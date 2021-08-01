eec4ef654f8e3c56fd9d9d2e87452d54995096d5
amarnarute <amarnarute2002@gmail.com>
2021-08-02 03:07:44 +0530

    Feat: Error checking

--- src/App.js
+++ src/App.js
@@ -1,62 +1,142 @@
-import {useState} from 'react'
-import classes from './app.module.css'
+import { useState } from 'react';
+import classes from './app.module.css';
 function App() {
+  const [bill, setBill] = useState(0);
+  const [cash, setCash] = useState(0);
+  const [show, setShow] = useState(false);
+  const [notesNo, setNotesNo] = useState([0, 0, 0, 0, 0, 0, 0]);
+  const [msg, setMsg] = useState('');
 
-  const [bill, setBill] = useState(0)
-  const [cash, setCash] = useState(0)
+  const [tableClass, setTableClass] = useState(
+    `${classes.table} ${classes.visible}`
+  );
+
   const notes = [2000, 500, 100, 20, 10, 5, 1];
 
-  const onChangeBill = (e)=> {
-    setBill(e.target.value);  
-  }
+  let returnNotes = [];
 
-  const onChangeCash = (e)=> {
-    setCash(e.target.value)
-  }
+  const onChangeBill = (e) => {
+    setBill(e.target.value);
+  };
+
+  const onChangeCash = (e) => {
+    setCash(e.target.value);
+  };
 
   const calculateNotes = (e) => {
     e.preventDefault();
-    let balance = cash-bill;
-    let returnNotes = notes.map(note=> {
-      if(note>balance){
+    if (cash < bill) {
+      setMsg('Cash is less than bill amount.');
+      setTableClass(`${classes.table} ${classes.visible}`);
+      return;
+    }
+
+    if (cash === bill) {
+      setMsg('Excat change is given.');
+      setTableClass(`${classes.table} ${classes.visible}`);
+      return;
+    }
+
+    setMsg('');
+
+    let balance = cash - bill;
+    returnNotes = notes.map((note) => {
+      if (note > balance) {
         return 0;
       }
 
-      let noteNumber =  Math.floor(balance/note)
-        balance = balance-noteNumber*note;
-        return noteNumber;
-    })
+      let noteNumber = Math.floor(balance / note);
+      balance = balance - noteNumber * note;
+      return noteNumber;
+    });
 
-    console.log(returnNotes)
-  }
+    setNotesNo(returnNotes);
+    setTableClass(
+      show ? `${classes.table}` : `${classes.table} ${classes.visible}`
+    );
 
-  return <div className="App">
-    <div className={classes.mainForm}>
+    console.log(returnNotes);
+  };
 
-      <div className="bill">
-        <form action="" >
-        <label htmlFor="bill">Bill Amount</label>
-        <input type="number" onChange={onChangeBill}/>
-        <button className={classes.nxtbtn} type="submit">Next</button>
-        </form>
+  const changeVisibility = (e) => {
+    e.preventDefault();
+    if (bill < 1) {
+      setMsg('Enter valid bill amount');
+      return;
+    }
+
+    setMsg('');
+    setShow((prevState) => {
+      return !prevState;
+    });
+  };
+
+  const cashClass = show
+    ? `${classes.cash}`
+    : `${classes.cash} ${classes.visible}`;
+
+  const nxtBtnClass = !show
+    ? `${classes.nxtbtn}`
+    : `${classes.nxtbtn} ${classes.visible}`;
+
+  return (
+    <div className={classes.background}>
+      <div className="App">
+        <div className={classes.mainForm}>
+          <div className="bill">
+            <form>
+              <label htmlFor="bill">Bill Amount</label>
+              <input type="number" onChange={onChangeBill} />
+              <button
+                className={nxtBtnClass}
+                type="submit"
+                onClick={changeVisibility}
+              >
+                Next
+              </button>
+            </form>
+          </div>
+
+          <div className={cashClass}>
+            <form onSubmit={calculateNotes}>
+              <label htmlFor="cash">Cash Amount</label>
+              <input type="number" onChange={onChangeCash} />
+              <button className={classes.chkbtn} type="submit">
+                Check
+              </button>
+            </form>
+          </div>
+
+          <div>{msg}</div>
+
+          <div className={tableClass}>
+            <label>Return amount:</label>
+            <h1>{cash - bill < 1 ? 0 : cash - bill}</h1>
+            <table>
+              <tbody>
+                <tr>
+                  <th>Notes</th>
+                  <td>2000</td>
+                  <td>500</td>
+                  <td>100</td>
+                  <td>20</td>
+                  <td>10</td>
+                  <td>5</td>
+                  <td>1</td>
+                </tr>
+                <tr>
+                  <th>Number</th>
+                  {notesNo.map((no) => {
+                    return <td>{no}</td>;
+                  })}
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
-      
-
-      <div className="cash">
-        <form action="" onSubmit={calculateNotes}>
-        <label htmlFor="cash">Cash Amount</label>
-        <input type="number" onChange={onChangeCash}/>
-        <button className={classes.chkbtn} type="submit">Check</button>
-        </form>
-      </div>
-
-      <div>
-
-      </div>
-      <h1>{bill-cash}</h1>
-
     </div>
-  </div>;
+  );
 }
 
 export default App;
